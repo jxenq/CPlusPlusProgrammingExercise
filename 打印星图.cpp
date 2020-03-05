e8d8b6625f1cd24233a45e71c73705753ea3090b
Z111D <57184694+Z111D@users.noreply.github.com>
2020-03-05 18:11:28 +0800

    Add files via upload

---
+++ 打印星图.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int space = 7,i,j;
+	for(i = 0;i < 4;i++)
+	{
+		for(j = 0;j < 30;j++)
+		{
+			cout << " ";	
+		}
+		for(j = 0;j < space - 1;j++)
+		{
+			cout << " ";
+		}
+		for(j = 0;j <= 7 - space;j++)
+		{
+			cout << "*";
+		}
+		cout << '\n';
+		space -= 2;
+	}
+	space = 5;
+	for(i = 0;i < 3;i++)
+	{
+		for(j = 0;j < 30;j++)
+		{
+			cout << " ";	
+		}
+		for(j = 0;j < space;j++)
+		{
+			cout << "*";
+		}
+		for(j = 0;j < 7 - space;j++)
+		{
+			cout << " ";
+		}
+		cout << '\n';
+		space -= 2;
+	}
+}
+
+/*                                  *                                  
+								  ***
+                                *****
+                              *******
+                              *****
+                              ***
+                              * 
+*/
+
+
